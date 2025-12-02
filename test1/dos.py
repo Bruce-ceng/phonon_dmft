efdc30cf427db2c7d7f0d9d8b4f136a3a4e475ab
bruce ceng <yanchengceng4@gmail.com>
2025-12-02 08:13:58 +0800

    加入註解

--- test1/dos.py
+++ test1/dos.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def semicircle(x,t):
+    '''
+    Define the semicircle density of state function of Bethe lattice 
+    by using Wigner semicircle distribution.
+    Input:
+        t:hopping amplitude
+        x:energy
+    Ouput:
+        np:density of state
+
+    '''
     return np.sqrt((2*t)**2-x**2)/(2*t**2*np.pi)
 
 if __name__=='__main__':
